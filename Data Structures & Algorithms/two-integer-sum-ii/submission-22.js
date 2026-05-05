bae34838c4ff06f1246bdcb5bda7d9c9c24d4328
Konrad Stasiuk <8351158+DonKonraddo@users.noreply.github.com>
2026-05-05 09:45:10 +0200

    Add: two-integer-sum-ii - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-22.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let left = 0;
+        let right = numbers.length - 1;
+
+        while (left < right) {
+            const sum = numbers[left] + numbers[right];
+            if (sum === target) {
+                return [left + 1, right + 1];
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return [];
+    }
+
+    twoSumOld(numbers, target) {
+        for (const [i, value] of numbers.entries()) {
+            const targetValue = target - value;
+
+            for (const [j, value2] of numbers.entries()) {
+                if (i === j) continue;
+                if (targetValue === value2) {
+                    return [i + 1, j + 1];
+                }
+            }
+        }
+    }
+}
